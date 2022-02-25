369018cc01af3647513744f0608a1889da288d9a
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-02-25 18:47:57 -0300

    aula 2

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.texto}>Olá Mundo!</Text>
+      <View style={styles.bloco}>
+        <Text style={styles.textoBloco}> Valor 1</Text>
+        <TextInput 
+            style={styles.input}
+            keyboardType="numeric"
+        />
+      </View>
+      <View style={styles.bloco}>
+        <Text style={styles.textoBloco}> Valor 2</Text>
+        <TextInput 
+            style={styles.input}
+            keyboardType="numeric"
+        />
+      </View>
+      <View style={styles.bloco}>
+        <TouchableOpacity style={styles.botao}>
+          <Text style={styles.textoBotao}>Somar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -12,12 +31,35 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#6495ED',
+    backgroundColor: '#6959CD',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textoBloco:{
+     fontSize:20,
   },
   texto:{
      color:'#FFF',
      fontSize: '30px'
-  }
+  },
+   input:{
+    borderColor:'#000',
+    borderWidth:2,
+    fontSize:30,
+    width:'80%'
+   },
+   bloco:{
+     width:'100%',
+     alignItems:'center',
+     marginTop:20
+   },
+   botao:{
+      backgroundColor:'#6495ED',
+      width:'80%',
+      textAlign:'center'
+   },
+   textoBotao:{
+      color:"#fff",
+      fontSize:30
+   }
 });
